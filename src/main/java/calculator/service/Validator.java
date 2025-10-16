1773f54fda5c16a10de56357446bf3fdb089a05d
K-iise <91732817+K-iise@users.noreply.github.com>
2025-10-16 16:24:55 +0900

    refacotr: 문자 검증 기능 분리

---
+++ src/main/java/calculator/service/Validator.java
@@ -0,0 +1,24 @@
+package calculator.service;
+
+public class Validator {
+
+    /**
+     * 빈 문자열 및 공백 처리
+     */
+    public boolean IsBlank(String text){
+        return text == null || text.isBlank();
+    }
+
+    /**
+     * 숫자 판별
+     */
+    public boolean IsInteger(String candidate){
+        return candidate.matches("^\\d+$");
+    }
+
+    public void IsException(String text){
+        if (!IsInteger(text)) {
+            throw new IllegalArgumentException("입력은 양의 정수여야 합니다.");
+        }
+    }
+}
--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -3,7 +3,6 @@ package calculator.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-
     public static String getIntput() {
         System.out.println("덧셈할 문자열을 입력해주세요.");
         return Console.readLine();
