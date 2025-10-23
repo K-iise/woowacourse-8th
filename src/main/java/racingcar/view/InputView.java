23bff6b4a9638beb5d98c35be1c06bb6be3b5ff2
K-iise <91732817+K-iise@users.noreply.github.com>
2025-10-23 19:59:34 +0900

    refactor: InputView 메소드 정리

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -3,13 +3,17 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
+  private final String CAR_NAME_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+  private final String TRY_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+
   public String getCarName() {
-    System.out.println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)");
+    System.out.println(CAR_NAME_MESSAGE);
     return Console.readLine();
   }
 
   public String getTryCount() {
-    System.out.println("시도할 횟수는 몇 회인가요?");
+    System.out.println(TRY_COUNT_MESSAGE);
     return Console.readLine();
   }
 }
