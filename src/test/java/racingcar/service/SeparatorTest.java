779bfee428be251576022375ee9c805a8d8958d4
K-iise <91732817+K-iise@users.noreply.github.com>
2025-10-23 23:54:18 +0900

    test(SeparatorTest): 자동차 이름 구분 테스트 추가

---
+++ src/test/java/racingcar/service/SeparatorTest.java
@@ -0,0 +1,23 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SeparatorTest {
+
+    @Test
+    public void 자동차_이름_구분() {
+        // given
+        String input = "pobi,woni,jun";
+        List<String> testNames = new ArrayList<>(Arrays.asList("pobi", "woni", "jun"));
+
+        // when
+        List<String> CarNames = Separator.parseCarNames(input);
+
+        // then
+        Assertions.assertThat(CarNames).isEqualTo(testNames);
+    }
+}
--- src/test/java/racingcar/view/InputViewTest.java
+++ src/test/java/racingcar/view/InputViewTest.java
@@ -9,38 +9,38 @@ import org.junit.jupiter.api.Test;
 
 public class InputViewTest {
 
-  @AfterEach
-  public void tearDown() {
-    Console.close();
-  }
+    @AfterEach
+    public void tearDown() {
+        Console.close();
+    }
 
-  @Test
-  public void 자동차_이름_입력() {
-    //given
-    String input = "pobi,woni,jun";
-    InputStream fakeInputStream = new ByteArrayInputStream(input.getBytes());
-    System.setIn(fakeInputStream);
-    InputView inputView = new InputView();
+    @Test
+    public void 자동차_이름_입력() {
+        //given
+        String input = "pobi,woni,jun";
+        InputStream fakeInputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(fakeInputStream);
+        InputView inputView = new InputView();
 
-    //when
-    String carsname = inputView.getCarName();
+        //when
+        String carsname = inputView.getCarName();
 
-    //then
-    Assertions.assertThat(carsname).isEqualTo("pobi,woni,jun");
-  }
+        //then
+        Assertions.assertThat(carsname).isEqualTo("pobi,woni,jun");
+    }
 
-  @Test
-  public void 시도_횟수_입력() {
-    // given
-    String input = "5";
-    InputStream fakeStream = new ByteArrayInputStream(input.getBytes());
-    System.setIn(fakeStream);
-    InputView inputView = new InputView();
+    @Test
+    public void 시도_횟수_입력() {
+        // given
+        String input = "5";
+        InputStream fakeStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(fakeStream);
+        InputView inputView = new InputView();
 
-    // when
-    String count = inputView.getTryCount();
+        // when
+        String count = inputView.getTryCount();
 
-    // then
-    Assertions.assertThat(count).isEqualTo("5");
-  }
+        // then
+        Assertions.assertThat(count).isEqualTo("5");
+    }
 }
