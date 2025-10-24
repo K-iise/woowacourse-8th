35687b083297ab95b9f083a75c971313130d18ec
K-iise <91732817+K-iise@users.noreply.github.com>
2025-10-24 14:32:47 +0900

    refactor(Validator): checkOverLength() 메소드 정리
    메소드 내부의 길이 검사를 validateNameLength() 메소드로 분리 함

--- src/main/java/racingcar/service/Validator.java
+++ src/main/java/racingcar/service/Validator.java
@@ -3,12 +3,18 @@ package racingcar.service;
 import java.util.List;
 
 public class Validator {
-    
+
+    private final String InVALID_NAME_LENGTH_MESSAGE = "이름은 5자를 초과할 수 없습니다.";
+
     public void checkOverLength(List<String> names) {
         for (String name : names) {
-            if (name.length() > 5) {
-                throw new IllegalArgumentException("이름은 5자를 초과할 수 없습니다.");
-            }
+            validateNameLength(name);
+        }
+    }
+
+    private void validateNameLength(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException(InVALID_NAME_LENGTH_MESSAGE);
         }
     }
 }
