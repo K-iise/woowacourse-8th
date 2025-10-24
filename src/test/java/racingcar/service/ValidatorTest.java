fd51f1f98b791c19ae14c551f3f4308b27e58aa7
K-iise <91732817+K-iise@users.noreply.github.com>
2025-10-24 14:19:49 +0900

    test(ValidatorTest): 이름 길이 검사 테스트 추가
    이름 5자 초과 예외, 이름 5자 이하 정상 통과 테스트로 구분

---
+++ src/test/java/racingcar/service/ValidatorTest.java
@@ -0,0 +1,33 @@
+package racingcar.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ValidatorTest {
+    
+    @Test
+    public void 이름_5자_초과_예외_발생() {
+        // given
+        List<String> names = new ArrayList<>(List.of("pobias", "woniss", "junjss"));
+        Validator validator = new Validator();
+
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            validator.checkOverLength();
+        });
+    }
+
+    @Test
+    public void 이름_5자_이하_정상_통과() {
+        // given
+        List<String> names = new ArrayList<>(List.of("pobi", "woni", "jun"));
+        Validator validator = new Validator();
+
+        // when & then
+        Assertions.assertDoesNotThrow(() -> {
+            validator.checkOverLength();
+        });
+    }
+}
