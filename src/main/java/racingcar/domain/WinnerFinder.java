9253bd29dd7f12844030b08f08c035d74899caa5
K-iise <91732817+K-iise@users.noreply.github.com>
2025-10-26 00:54:12 +0900

    refactor(WinnerFinder): findWinners() 메소드 정리
    가장 큰 가중치를 구하는 메소드를 분리하고, 그 값과 같은 객체를 필터링한 리스트를 반환함

--- src/main/java/racingcar/domain/WinnerFinder.java
+++ src/main/java/racingcar/domain/WinnerFinder.java
@@ -1,25 +1,15 @@
 package racingcar.domain;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class WinnerFinder {
     public List<Car> findWinners(List<Car> cars) {
+        int max = getMaxWeight(cars);
+        return cars.stream().filter(car -> car.getWeight() == max).collect(Collectors.toList());
+    }
 
-        int max = 0;
-        for (Car car : cars) {
-            if (car.getWeight() > max) {
-                max = car.getWeight();
-            }
-        }
-        List<Car> winners = new ArrayList<>();
-
-        for (Car car : cars) {
-            if (car.getWeight() == max) {
-                winners.add(car);
-            }
-        }
-
-        return winners;
+    private int getMaxWeight(List<Car> cars) {
+        return cars.stream().mapToInt(Car::getWeight).max().orElse(0);
     }
 }
