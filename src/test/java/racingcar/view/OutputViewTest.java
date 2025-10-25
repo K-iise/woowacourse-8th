ed3eece5a565638c480437293d507bbd0cb46106
K-iise <91732817+K-iise@users.noreply.github.com>
2025-10-25 16:52:43 +0900

    test(OutputViewTest): 자동차 진행 상태 출력 테스트 추가

---
+++ src/test/java/racingcar/view/OutputViewTest.java
@@ -0,0 +1,41 @@
+package racingcar.view;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.Car;
+
+public class OutputViewTest {
+
+    private ByteArrayOutputStream outputStream;
+
+    @BeforeEach
+    public void setUpStearm() {
+        outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void 자동차_진행_상태_출력() {
+        // given
+        OutputView outputView = new OutputView();
+        List<Car> cars = new ArrayList<>(List.of(new Car("yun"), new Car("cobi")));
+        cars.get(1).addWeight(5);
+
+        // when
+        outputView.printProcess(cars);
+
+        // then
+        Assertions.assertEquals("yun : \n" + "cobi : -\n", outputStream.toString());
+    }
+}
