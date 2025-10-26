451cbe2745d02759a99ddf39fba3f07667debe42
K-iise <91732817+K-iise@users.noreply.github.com>
2025-10-26 18:15:03 +0900

    test(CarGeneratorTest): 자동차 생성 테스트 추가

---
+++ src/test/java/racingcar/domain/CarGeneratorTest.java
@@ -0,0 +1,33 @@
+package racingcar.domain;
+
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class CarGeneratorTest {
+    @Test
+    public void 자동차_생성() {
+        // given
+        CarGenerator carGenerator = new CarGenerator();
+        String cars = "cobi,james,top";
+        List<Car> testList = List.of(new Car("cobi"), new Car("james"), new Car("top"));
+
+        // when
+        List<Car> carList = carGenerator.createCars(cars);
+
+        // then
+        Assertions.assertEquals(testList, carList);
+    }
+
+    @Test
+    public void 자동차_생성_예외_발생() {
+        // given
+        CarGenerator carGenerator = new CarGenerator();
+        String cars = "cobias,james,top";
+        List<Car> testList = List.of(new Car("cobias"), new Car("james"), new Car("top"));
+
+        // when & then
+        Assertions.assertThrows(IllegalArgumentException.class, () -> carGenerator.createCars(cars));
+    }
+}
+
