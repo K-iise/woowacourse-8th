a18ef206a4c71a28289def2a0ea39deb31f28eb4
K-iise <91732817+K-iise@users.noreply.github.com>
2025-10-30 21:17:51 +0900

    feat: 구입 금액 단위 검증 기능 구현

--- src/main/java/lotto/domain/LottoPurchase.java
+++ src/main/java/lotto/domain/LottoPurchase.java
@@ -7,11 +7,17 @@ public class LottoPurchase {
     private final int amount;
 
     public LottoPurchase(int amount){
+        validateAmount(amount);
         this.amount = amount;
     }
 
     public int getAmount() {
         return this.amount;
+    }
+
+    public void validateAmount(int amount){
+        if (amount % PURCHASE_PRICE != 0)
+            throw new IllegalArgumentException("[ERROR] 구입 금액은 1,000원 단위로 입력받아야 합니다.");
     }
 
 }
