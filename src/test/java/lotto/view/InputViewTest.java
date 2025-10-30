747fc61f3932e96acbf535056d791dbf885c20ae
K-iise <91732817+K-iise@users.noreply.github.com>
2025-10-30 15:51:17 +0900

    test: 로또 구입 금액 입력 테스트 작성

---
+++ src/test/java/lotto/view/InputViewTest.java
@@ -0,0 +1,32 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InputViewTest {
+
+    @AfterEach
+    public void tearDown(){
+        Console.close();
+    }
+
+    @Test
+    public void 로또_구입_금액_입력(){
+        // given
+        String input = "14000";
+        InputStream fakeInputStream = new ByteArrayInputStream(input.getBytes());
+        System.setIn(fakeInputStream);
+
+        InputView inputView = new InputView();
+
+        // when
+        String lottoPrice = inputview.getLottoPrice();
+
+        // then
+        Assertions.assertEquals(input, lottoPrice);
+    }
+}
