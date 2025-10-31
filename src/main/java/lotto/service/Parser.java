8f350624aa205ddc31e14159436ddb380a010680
K-iise <91732817+K-iise@users.noreply.github.com>
2025-10-31 22:23:56 +0900

    refactor: 당첨 번호 구분 기능 리팩토링

--- src/main/java/lotto/service/Parser.java
+++ src/main/java/lotto/service/Parser.java
@@ -1,34 +1,49 @@
 package lotto.service;
 
-
-import java.util.Collections;
+import java.util.Arrays;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 public class Parser {
-    public int changeInteger(String purchaseAmount){
+    public int changeInteger(String purchaseAmount) {
         try {
             return Integer.parseInt(purchaseAmount);
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             throw new IllegalArgumentException("[ERROR] 구입 금액은 숫자여야 합니다.");
         }
     }
 
-    public TreeSet<Integer> separateNumber(String input){
-        TreeSet<Integer> lotteryNumber = new TreeSet<>();
-        String [] numberList = input.split(",");
-
-        for (String number : numberList){
-            try {
-                int lotteryNum = Integer.parseInt(number);
-                if (lotteryNum > 45) throw new IllegalArgumentException("[ERROR] 당첨 번호는 1 ~ 45 범위에 속해야 합니다.");
-                lotteryNumber.add(lotteryNum);
-            } catch (IllegalArgumentException e){
-                throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 합니다.");
-            }
-        }
-
-        if (lotteryNumber.size() != 6) throw new IllegalArgumentException("[ERROR] 당첨 번호는 중복될 수 없습니다.");
-
+    public TreeSet<Integer> separateNumber(String input) {
+        TreeSet<Integer> lotteryNumber = parseToNumbers(input);
+        validateNumbers(lotteryNumber);
         return new TreeSet<>(lotteryNumber);
+    }
+
+    private TreeSet<Integer> parseToNumbers(String input) {
+        try {
+            return Arrays.stream(input.split(","))
+                    .map(String::trim)
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toCollection(TreeSet::new));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 숫자여야 합니다.");
+        }
+    }
+
+    private void validateNumbers(TreeSet<Integer> numbers) {
+        validateCount(numbers);
+        numbers.forEach(this::validateRange);
+    }
+
+    private void validateRange(int number) {
+        if (number > 45 || number < 1) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 1 ~ 45 범위에 속해야 합니다.");
+        }
+    }
+
+    private void validateCount(TreeSet<Integer> numbers) {
+        if (numbers.size() != 6) {
+            throw new IllegalArgumentException("[ERROR] 당첨 번호는 6개여야 하며 중복될 수 없습니다.");
+        }
     }
 }
