e025241ffa604c6c732bd282134ad1526e9c79d1
K-iise <91732817+K-iise@users.noreply.github.com>
2025-11-01 18:13:25 +0900

    test: 당첨번호 및 보너스 번호 비교 테스트 추가

--- src/test/java/lotto/domain/WinningLottoTest.java
+++ src/test/java/lotto/domain/WinningLottoTest.java
@@ -24,4 +24,31 @@ public class WinningLottoTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    public void 당첨_비교_테스트() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 3, 12, 44, 23, 34));
+        int bonusNumber = 4;
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+        Lotto testLotto = new Lotto(List.of(1, 10, 12, 33, 35, 42));
+
+        // when
+        int count = winningLotto.matchLotto(testLotto);
+
+        // then
+        Assertions.assertThat(count).isEqualTo(2);
+    }
+
+    @Test
+    public void 보너스_번호_비교_테스트() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 3, 12, 44, 23, 34));
+        int bonusNumber = 4;
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+        Lotto testLotto = new Lotto(List.of(1, 10, 12, 4, 35, 42));
+
+        // when & then
+        org.junit.jupiter.api.Assertions.assertTrue(winningLotto.matchBonusNumber(testLotto));
+    }
+
 }
