80fa4b81653babb18bfcc1f978a08cd3fe78b084
K-iise <91732817+K-iise@users.noreply.github.com>
2025-11-02 00:59:35 +0900

    test: 발행한 로또 번호 출력 테스트 추가

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -1,6 +1,8 @@
 package lotto.view;
 
 import lotto.domain.LottoPurchase;
+import lotto.domain.Lottos;
+import lotto.service.LottoService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +16,22 @@ public class OutputViewTest {
         Assertions.assertThatCode(() -> outputView.printLottoPurchase(lottoPurchase))
                 .doesNotThrowAnyException();
 
+    }
+
+    @Test
+    public void 발행한_로또_번호_출력() {
+        // given
+        OutputView outputView = new OutputView();
+        LottoPurchase lottoPurchase = new LottoPurchase(8000);
+        LottoService lottoService = new LottoService();
+
+        // when
+        Lottos lottos = new Lottos(lottoService.generateLottos(lottoPurchase.getLottoCount()));
+
+        // then
+        Assertions.assertThatCode(() -> outputView.printLottos(lottos))
+                .doesNotThrowAnyException();
+        
     }
 
 }
