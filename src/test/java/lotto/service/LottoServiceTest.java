6d19128e294e160a5ee37aa7e9d93e9a3fdb9ba8
K-iise <91732817+K-iise@users.noreply.github.com>
2025-11-02 01:40:10 +0900

    test: 로또 등수 판정 테스트 추가

--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -1,6 +1,9 @@
 package lotto.service;
 
+import java.util.List;
+import lotto.domain.Lotto;
 import lotto.domain.LottoPurchase;
+import lotto.domain.WinningLotto;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,6 +16,21 @@ public class LottoServiceTest {
 
         // when & then
         Assertions.assertThatCode(() -> lottoService.generateLottos(lottoPurchase.getLottoCount()))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    public void 로또_등수_판정_정상_테스트() {
+        // given
+        Lotto lotto = new Lotto(List.of(1, 3, 12, 44, 23, 34));
+        int bonusNumber = 4;
+        WinningLotto winningLotto = new WinningLotto(lotto, bonusNumber);
+        Lotto test = new Lotto(List.of(1, 3, 12, 44, 4, 43));
+
+        LottoService lottoService = new LottoService();
+
+        // when & then
+        Assertions.assertThatCode(() -> lottoService.judgeRank(winningLotto, lotto))
                 .doesNotThrowAnyException();
     }
 }
