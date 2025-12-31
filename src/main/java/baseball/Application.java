5e2dbf7cd2af386e3fb543cbd8a33959c015036d
K-iise <91732817+K-iise@users.noreply.github.com>
2025-12-31 22:31:12 +0900

    feat: 숫자 입력 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.view.InputView;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        InputView inputView = new InputView();
+        String numbers = inputView.getNumbers();
+
     }
 }
