34deda0e5682e1ba0324abe35df17da50e5bd499
K-iise <91732817+K-iise@users.noreply.github.com>
2025-12-31 22:40:05 +0900

    feat: 숫자 범위 검증 추가

--- src/main/java/baseball/service/Validator.java
+++ src/main/java/baseball/service/Validator.java
@@ -1,10 +1,21 @@
 package baseball.service;
 
 public class Validator {
+
+    private final String range = "^[0-9]*$";
+
     public void checkNumberLength(String numbers){
         if (numbers.length() != 3){
             throw new IllegalArgumentException("[Error] 입력된 숫자의 길이가 3이 아닙니다.");
         }
     }
-    
+
+    public void checkNumberInteger(String numbers){
+        for (int i = 0; i < numbers.length(); i++) {
+            int number = numbers.charAt(i) - '0';
+            if (!(number > 0 && number < 9)){
+                throw new IllegalArgumentException("[Error] 입력된 숫자의 범위는 1~9여야 합니다.");
+            }
+        }
+    }
 }
