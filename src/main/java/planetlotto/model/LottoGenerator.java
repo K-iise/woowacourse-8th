625dafd0058b65352650dacefcccf2f0707181cc
K-iise <91732817+K-iise@users.noreply.github.com>
2026-01-10 16:44:21 +0900

    feat: 로또 생성 정렬 추가

--- src/main/java/planetlotto/model/LottoGenerator.java
+++ src/main/java/planetlotto/model/LottoGenerator.java
@@ -2,6 +2,8 @@ package planetlotto.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class LottoGenerator {
@@ -9,9 +11,15 @@ public class LottoGenerator {
     public static List<List<Integer>> createLotto(PurchaseInfo purchaseInfo){
         List<List<Integer>> lottoList = new ArrayList<>();
         for (int i = 0; i < purchaseInfo.getQuantity(); i++) {
-            lottoList.add(generateLotto());
+            lottoList.add(sortNumber(generateLotto()));
         }
         return lottoList;
+    }
+
+    private static List<Integer> sortNumber(List<Integer> numbers) {
+        List<Integer> sorted = new ArrayList<>(numbers);
+        sorted.sort(Comparator.naturalOrder());
+        return sorted;
     }
 
     private static List<Integer> generateLotto() {
