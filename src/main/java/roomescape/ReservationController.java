84dcfdda6a9be8ca6503c28e6f462e0fabfba8d2
K-iise <kimyungi1004@naver.com>
2026-04-29 04:03:14 +0900

    feat: 예약 조회 API 구현

---
+++ src/main/java/roomescape/ReservationController.java
@@ -0,0 +1,20 @@
+package roomescape;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReservationController {
+
+    private List<Reservation> reservations = new ArrayList<>();
+    private AtomicLong index = new AtomicLong(1);
+
+    @GetMapping("/reservations")
+    public ResponseEntity<List<Reservation>> getReservation(){
+        return ResponseEntity.ok().body(reservations);
+    }
+}
