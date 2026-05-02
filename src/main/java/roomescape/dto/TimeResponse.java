87e07088ec7711eb7687e2b3417ca5eda00a59de
K-iise <kimyungi1004@naver.com>
2026-05-02 12:04:32 +0900

    fix: TimeResponse 요구사항 네이밍 수정

--- src/main/java/roomescape/dto/TimeResponse.java
+++ src/main/java/roomescape/dto/TimeResponse.java
@@ -5,19 +5,19 @@ import roomescape.model.ReservationTime;
 
 public class TimeResponse {
     private Long id;
-    private LocalTime timeAt;
+    private LocalTime startAt;
 
     public Long getId() {
         return id;
     }
 
-    public LocalTime getTimeAt() {
-        return timeAt;
+    public LocalTime getStartAt() {
+        return startAt;
     }
 
-    public TimeResponse(Long id, LocalTime timeAt) {
+    public TimeResponse(Long id, LocalTime startAt) {
         this.id = id;
-        this.timeAt = timeAt;
+        this.startAt = startAt;
     }
 
     public static TimeResponse from(ReservationTime reservationTime) {
